d6893c211bc24a12a157b3957341eaa91fb5f020
Roje <teahyeon2009@naver.com>
2024-06-09 18:53:48 +0900

    Initial Commit

---
+++ src/main/java/xyz/chide1/rojeshop/RojeShop.java
@@ -0,0 +1,17 @@
+package xyz.chide1.rojeshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RojeShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+
+}
